8834ec8cfb756c36d9adc4aa46b5c05c1a0b7c64
ruhi <raktterruhi@gmail.com>
2022-06-26 01:12:30 +0600

    testimonials added

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './Testimonials.css';
+
 
 const Testimonials = () => {
     return (
-        <div>
-            <h1>reviews</h1>
+        <div className='testimonials'>
+            <div>
+                <h3>Testimonials</h3>
+                <p>
+                    “I felt compelled to compliment the freshness of your peppercorns. I
+                    do not think I have ever smelled such fragrance when opening a package
+                    of them.”
+                </p>
+                <p>“All I can say is that if my wife would give me a bigger budget, I would have
+                    buy out your store.”
+                </p>
+                <p>
+                    “I love the uniqueness of your spice blends! Looking forward to using them in many
+                    dishes this spring and summer!”
+                </p>
+                <p>
+                    “I have tried all three spices that I purchased and they bring a delightful accent to 
+                    the food.”
+                </p>
+                <p>“Love the spices. Vibrant colors and flavor!”</p>
+            </div>
+            <div>
+                <img src="https://i.ibb.co/CHWv0Qv/slide2.jpg" alt="" />
+            </div>
         </div>
     );
 };
