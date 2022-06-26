ed69714b80f06aae22c9d414f8c82db55b1ebde9
ruhi <raktterruhi@gmail.com>
2022-06-26 18:26:56 +0600

    recipes component added

--- src/Components/Recipes/Recipes.js
+++ src/Components/Recipes/Recipes.js
@@ -1,9 +1,179 @@
 import React from 'react';
+import './Recipes.css';
 
 const Recipes = () => {
     return (
-        <div>
-            <h3>recipes</h3>
+        <div className="container">
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
+            <div className="recipe">
+                <div className="recipe-img">
+                    <img src="https://i.ibb.co/74YF0Wc/image.png" alt="" />
+                </div>
+                <div className="recipe-info">
+                    <h5>Nashville Air-Fried Hot Chicken Wings recipe</h5>
+                    <p>
+                    Crispy on the outside, juicy on the inside, and air-fried to
+                    perfection, these chicken wings are brilliantly seasoned and sure
+                    to appeal to any chicken wing enthusiast. Under the expert hand of
+                    Chef Marc Dix, these chicken wings are prepared with liquid brine
+                    and cooked in an air fryer to create a tender and flavorful wing
+                    with very crispy, perfectly-browned skin.
+                    </p>
+                    <a href="#full-recipe"> Click for recipe</a>
+                </div>
+            </div>
         </div>
     );
 };
