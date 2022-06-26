823774d1924462a2332ea1956545ac8ecb3da10c
ruhi <raktterruhi@gmail.com>
2022-06-26 18:48:21 +0600

    404 added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h3>Page Unavailable</h3>
+        <div className='not-found mt-5'>
+            <h3>Opps page is unavailable!!!</h3>
+            <img src="https://i.ibb.co/ns3NZHW/error.jpg" alt="" />
         </div>
     );
 };
